6c7aa59d4169741bc9b8bd63a111aed8c7885596
e-emd <dewin.emd@gmail.com>
2021-02-11 16:38:49 -0700

    initial

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+
+export default function Page() {
+  return (
+    <div>
+      <GlobalStyles />
+      <Header />
+      <h1>I am the page component</h1>
+    </div>
+  );
+}
